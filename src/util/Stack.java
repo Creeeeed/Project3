a70b42410490d79bc0200af685b015f41166f4a8
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-10-14 08:03:34 -0600

    Initial commit

---
+++ src/util/Stack.java
@@ -0,0 +1,23 @@
+package util;
+//<Type> name can be anything
+import
+public class Stack <Type> {
+    private Type[ ] data;
+
+    public void push(Type item){
+
+    }
+    public Type pop(){
+
+    }
+    public int size(){
+
+    }
+    void grow(){
+
+    }
+    boolean requiresGrowth(){
+
+    }
+
+}
---
+++ src/util/StackTest.java
@@ -0,0 +1,11 @@
+package util;
+import java.util.Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack<String> item
+
+    }
+
+}
+//https://www.programiz.com/java-programming/stack
