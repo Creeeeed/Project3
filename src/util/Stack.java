d9885f6dc74250bb658b46c0925f82b99064dc4c
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-10-18 18:14:10 -0600

    Finshed

--- src/util/Stack.java
+++ src/util/Stack.java
@@ -1,25 +1,59 @@
 package util;
-//<Type> name can be anything
-import
-public class Stack <Type> {
-    private Type[] data;
 
-    public void push(Type item){
+public class Stack
+{
+    public int stack[] = new int[5];
+    int top = 0;
+
+
+    public void push(int data){
+        if (top==5){
+            System.out.println("Stack is full");
+        }
+        else
+        {
+        stack[top] = data;
+        top++;
+        }
+        //increments
+
 
     }
-    public Type pop(){
+    public int pop()
+    {
+        int data = 0;
+
+        if (requiresGrowth())
+        {
+            System.out.println("Stack is empty");
+        }
+        else
+        {
+            top--;
+            data = stack[top];
+            stack[top] = 0;
+        }
+        return data;
+    }
+    public int size()
+    {
+        return top;
+    }
+
+    public boolean requiresGrowth()
+    {
+        return top<= 0;
+    }
+
+    public void show(){
+        for(int n : stack)
+        {
+            System.out.println((n + " "));
+        }
 
     }
-    public int size(){
-
-    }
-    void grow(){
-
-    }
-    boolean requiresGrowth(){
-
-    }
-
 }
 
-//https://gist.github.com/anil477/1ad6cab89b4188f675a7ce1188bbaa34
+//References
+//https://www.youtube.com/watch?v=fptlqsesjxY
+//https://www.youtube.com/watch?v=fptlqsesjxY
